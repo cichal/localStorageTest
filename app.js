6b956614dfab8151fe65fc2abb7592003c47848e
Alex Cich <cichal13@gmail.com>
2019-01-16 15:57:17 -0700

    Added a filter to sort by those who have responded

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
 
 function createLi(text) {
   const li = document.createElement('li');
