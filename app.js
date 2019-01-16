9d4874955916cd6947a8859ff541ab212035f9a0
Alex Cich <cichal13@gmail.com>
2019-01-16 15:16:05 -0700

    Added functionality to filter and edit users

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = input.value;
-  input.value ='';
+function createLi(text) {
   const li = document.createElement('li');
-  li.textContent = text
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
   const checkbox = document.createElement('input');
@@ -15,10 +14,21 @@ form.addEventListener('submit', (e) => {
   label.appendChild(checkbox);
   li.appendChild(label);
 
-  const button = document.createElement('button');
-  button.textContent = 'remove';
-  li.appendChild(button);
-  
+  const editButton = document.createElement('button');
+  editButton.textContent = 'edit';
+  li.appendChild(editButton);
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  li.appendChild(removeButton);
+  return li;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value ='';
+  const li = createLi(text);
   ul.appendChild(li);
 });
 
@@ -31,5 +41,31 @@ ul.addEventListener('change', (e) => {
     listItem.className = 'responded';
   } else {
     listItem.className = '';
+  }
+});
+
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const button = e.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.textContent === 'remove') {
+      ul.removeChild(li);
+    } else if (button.textContent === 'edit') {
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      li.removeChild(span);
+      button.textContent = 'save';
+    } else if (button.textContent === 'save') {
+      const input = li.firstElementChild;
+      const span = document.createElement('span');
+      span.textContent = input.value;
+      li.insertBefore(span, input);
+      li.removeChild(input);
+      button.textContent = 'edit';
+    }
   }
 });
