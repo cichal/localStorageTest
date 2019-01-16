ac9597caa619f970d26db5da3301d9025d6b7f24
Alex Cich <cichal13@gmail.com>
2019-01-16 16:47:57 -0700

    Finished functionality of filter.

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ filterCheckBox.type = 'checkbox';
 div.appendChild(filterLabel);
 div.appendChild(filterCheckBox);
 mainDiv.insertBefore(div, ul);
+filterCheckBox.addEventListener('change', (e) => {
+  const isChecked = e.target.checked;
+  const lis = ul.children;
+  if (isChecked) {
+    for (let i = 0; i < lis.length; i++) {
+      let li = lis[i];
+      if (li.className === 'responded') {
+        li.style.display = '';
+      } else {
+        li.style.display = 'none';
+      }
+    } 
+  } else {
+    for (let i = 0; i < lis.length; i++) {
+      let li = lis[i];
+      li.style.display = '';
+    }
+  }
+});
 
 function createLi(text) {
   const li = document.createElement('li');
